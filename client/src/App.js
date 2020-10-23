38f6c35501b5200c790d5687b4abc20c6cfc53d3
yash joshi <yjoshi.2098@gmail.com>
2020-10-23 16:03:02 +0530

    [TASK-1] conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         Prasiddha
-        yash
+        yash yash yash
         <a
           className="App-link"
           href="https://reactjs.org"
