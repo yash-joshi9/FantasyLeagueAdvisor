ee4c70ec5738e95b50035e4c7ded7dc9584350e3
Prasiddh <prasiddh79721@gmail.com>
2020-10-23 06:34:04 -0400

    [Task 1] conflict from prasiddh

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         Prasiddha
-        yash
+        yash prasiddh 
         <a
           className="App-link"
           href="https://reactjs.org"
