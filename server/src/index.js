91ba143c8900872cda561d0b3fcc7d4e0e244f5f
yash joshi <yjoshi.2098@gmail.com>
2020-10-25 16:27:33 +0530

    [TASK-1] setup server

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+require("./db/mongoose");
+const User = require("./models/User");
+const userRouter = require("./routers/user");
+
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(userRouter);
+
+console.log(">>>>>>")
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
